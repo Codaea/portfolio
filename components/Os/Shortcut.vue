f846503507416edb630f5e30a23c00bd2c64ec6e
Codaea <dakotajayroth@gmail.com>
2024-11-10 11:09:07 -0800

    removed redudant code

--- components/Os/Shortcut.vue
+++ components/Os/Shortcut.vue
@@ -1,5 +1,5 @@
 <template>
-    <div @click="" class="items-center w-[5.6rem] h-fit p-2 hover:cursor-pointer">
+    <div class="items-center w-[5.6rem] h-fit p-2 hover:cursor-pointer">
         <div class="w-full h-full relative p-2">
             <img class="absolute bottom-0 left-0 w-4" src="public/os/shortcut.png"/> <!-- TODO: make own version, this is CC.-->
             <img class="w-full object-cover" :src="imageSrc" />
@@ -22,9 +22,5 @@ const props = defineProps({
         required: true
     }
 })
-
-function handleClick() {
-    console.log('click TODO: open window');
-}
 
 </script>
