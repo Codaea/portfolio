c47353437a8115157bf7588be3c93e133dd64333
Codaea <dakotajayroth@gmail.com>
2024-12-27 01:24:25 -0800

    fixed hydration mismatch

--- pages/index.vue
+++ pages/index.vue
@@ -12,19 +12,21 @@
             href="https://owl.purdue.edu/owl/general_writing/grammar/articles_a_versus_an.html"
             >a(n)</a
           >
-          <RollingText />
+          <client-only>
+            <RollingText />
+          </client-only>
         </p>
       </div>
     </div>
     <div class="space-y-4 m-4">
       <h2 class="text-6xl">Things I've Done</h2>
-      <div v-for="post of data" class="flex flex-col">
+      <div v-for="post of data" class="flex flex-col space-x-4 w-full">
         <div
           class="w-1/4 min-w-64 p-4 bg-[#bac0ca] border-4 border-t-gray-100 border-l-gray-100 border-r-gray-600 border-b-gray-600 focus:border-black focus:border-t-2 focus:border-l-2"
         >
-        <NuxtLink :to="post._path">
-          <img class="rounded-md drop-shadow-lg" :src="post.image" />
-        </NuxtLink>
+          <NuxtLink :to="post._path">
+            <img class="rounded-md drop-shadow-lg" :src="post.image" />
+          </NuxtLink>
           <NuxtLink :to="post._path" class="text-xl">
             {{ post.title }}</NuxtLink
           >
@@ -37,6 +39,6 @@
 
 <script setup lang="ts">
 const { data } = useAsyncData('things', () =>
-  queryContent('/things').limit(3).find()
+  queryContent('/things').limit(3).sort({ date: 1, $numeric: true }).find()
 );
 </script>
