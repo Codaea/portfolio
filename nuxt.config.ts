74e00d2438299d2e91821961a80fba57fef2aa17
Codaea <dakotajayroth@gmail.com>
2024-12-28 12:54:00 -0800

    added shiki highlighting, formatted prettierrc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,5 +5,34 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxt/content',
-  ]
-})
+  ],
+  content: {
+    documentDriven: {
+      injectPage: false,
+    },
+    highlight: {
+      theme: {
+        default: 'github-light',
+        dark: 'github-dark',
+      },
+      langs: [
+        'typescript',
+        'bash',
+        'json',
+        'yaml',
+        'sql',
+      ]
+    }
+  },
+  tailwindcss: {
+    config: {
+      theme: {
+        extend: {
+          fontFamily: {
+            'w95': ['/public/w95a.woff2']
+          }
+        }
+      }
+    }
+  }
+});
